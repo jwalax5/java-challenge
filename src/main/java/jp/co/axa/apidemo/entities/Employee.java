19f7e02a9487f4e7d4ede745195872c4b2d9a686
jwalax5 <josh90810@gmail.com>
2024-05-14 21:01:58 +0900

    minor update of validation rule of Employee entity

--- src/main/java/jp/co/axa/apidemo/entities/Employee.java
+++ src/main/java/jp/co/axa/apidemo/entities/Employee.java
@@ -10,8 +10,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.*;
 
 @Entity
 @Getter
@@ -23,12 +22,14 @@ public class Employee {
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
 
-    @NotEmpty(message="please input employee name")
+    @NotBlank(message="please input employee name")
     @Column(name="EMPLOYEE_NAME")
     private String name;
 
     @NotNull(message="please input salary")
     @Column(name="EMPLOYEE_SALARY")
+    @Max(value = Integer.MAX_VALUE, message = "please input valid salary")
+    @Min(value = 0, message = "please input valid salary")
     private Integer salary;
 
     @NotEmpty(message="please input department")
